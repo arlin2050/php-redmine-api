c9f64cdbec3df1e7ce275ad7174d9b3aea33c7fb
André Cardoso <acardoso@orupaca.fr>
2014-07-11 11:56:18 +0200

    Custom fields value's modifications : cf https://github.com/arlin2050/redmine/commit/c5e070dcdab36e995b4aff52927c035aba250f63.

--- lib/Redmine/Api/CustomField.php
+++ lib/Redmine/Api/CustomField.php
@@ -67,4 +67,69 @@ class CustomField extends AbstractApi
 
         return $arr[(string) $name];
     }
+
+    /**
+     * Update custom fields value's by custom field number. Requires authentication.
+     *
+     * @param  string           $id     the issue number
+     * @param  array            $params
+     * @return SimpleXMLElement
+     */
+    public function update($id, array $params)
+    {
+        $defaults = array(
+            //'id'              => $id,
+            //'name'            => null,
+            //'customized_type' => null,
+            //'field_format'    => null,
+            //'regexp'          => null,
+            //'min_length'      => null,
+            //'max_length'      => null,
+            //'is_required'     => null,
+            //'is_filter'       => null,
+            //'searchable'      => null,
+            //'multiple'        => null,
+            //'default_value'   => null,
+            //'visible'         => null,
+            'possible_values' => null
+        );
+        $params = array_filter(array_merge($defaults, $params));
+
+        $xml = $this->buildXML($params);
+
+        return $this->put('/custom_fields/'.$id.'.xml', $xml->asXML());
+    }
+
+    /**
+     * Build the XML for a custom field
+     * @param  array            $params for the new/updated issue data
+     * @return SimpleXMLElement
+     */
+    private function buildXML(array $params = array())
+    {
+        $xml = new SimpleXMLElement('<?xml version="1.0"?><custom_field></custom_field>');
+
+        foreach ($params as $k => $v) {
+            if ('possible_values' === $k && is_array($v)) {
+                $this->attachPossibleValuesXML($xml, $v);
+            } else {
+                $xml->addChild($k, $v);
+            }
+        }
+
+        return $xml;
+    }
+
+    /**
+     * Attaches Custom Fields possible values
+     *
+     * @param  SimpleXMLElement $xml    XML Element the custom fields are attached to
+     * @param  array            $fields array of fields to attach, each field needs name, id and value set
+     * @return $xml             SimpleXMLElement
+     */
+    protected function attachPossibleValuesXML(SimpleXMLElement $xml, array $fields)
+    {
+        $_fields = $xml->addChild('possible_values', implode("\r\n",$fields));
+        return $xml;
+    }
 }
